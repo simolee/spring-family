7d78075f4f95634b31c82c1ff8f93eac9ddac4e8
simon <simo.lee168@gmail.com>
2019-04-08 15:35:20 +0800

    git ds demo

---
+++ ex2/datasource-demo/src/main/java/com/simon/sample/datasourcedemo/DatasourceDemoApplication.java
@@ -0,0 +1,47 @@
+package com.simon.sample.datasourcedemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Slf4j
+@SpringBootApplication
+public class DatasourceDemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatasourceDemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        showConnection();
+        showData();
+    }
+
+    private void showConnection() throws SQLException {
+        log.info("datasource: {}", dataSource.toString());
+        Connection conn = dataSource.getConnection();
+        log.info("connection: {}", conn.toString());
+        conn.close();
+    }
+
+    private void showData() {
+        jdbcTemplate.queryForList("select * from FOO")
+                .forEach(row -> log.info(row.toString()));
+    }
+
+
+}

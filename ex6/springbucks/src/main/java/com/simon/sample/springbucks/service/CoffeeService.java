060e44ac27470da718c6a00616ae25fcf157d0fd
simon <simo.lee168@gmail.com>
2019-04-15 16:54:52 +0800

    springbucks demo

--- ex6/springbucks/src/main/java/com/simon/sample/springbucks/service/CoffeeService.java
+++ ex6/springbucks/src/main/java/com/simon/sample/springbucks/service/CoffeeService.java
@@ -4,6 +4,7 @@ import com.simon.sample.springbucks.model.Coffee;
 import com.simon.sample.springbucks.repository.CoffeeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.joda.money.Money;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Sort;
@@ -16,6 +17,7 @@ import java.util.List;
 @CacheConfig(cacheNames = "CoffeeCache")
 public class CoffeeService {
 
+    @Autowired
     private CoffeeRepository coffeeRepository;
 
     public Coffee saveCoffee(String name, Money price) {

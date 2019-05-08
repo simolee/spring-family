ebd697c246798f47856ee74f8afe189ca6e43798
simon <simo.lee168@gmail.com>
2019-05-08 14:43:45 +0800

    consul demo

---
+++ ex12/consul-customer-service/src/test/java/com/simon/sample/springbucks/customer/ConsulCustomerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.simon.sample.springbucks.customer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConsulCustomerServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
